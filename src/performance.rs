ea2334a68b19b57e31eb1ce37a00b5d990d758cd
Jacob Root <otis@otisroot.com>
2021-11-22 09:29:42 -0800

    Start Perflog
    Signed-off-by: Jacob Root <otis@otisroot.com>

--- src/performance.rs
+++ src/performance.rs
@@ -1,3 +1,8 @@
 // Here is where I log performance by averaging CPU deltas for processes
 // Keep n deltas and move to a new delta every m, evict oldest deltas every once and a while too
-// Keep this in Memory OR fs
+// Index by hashmap and primitive strings - keep 2 deltas of 200 each (so a tuple)
+// Keep this in Memory to aid scheduling, but offload to fs if bucket permitting (make fs offload a process with low prio)
+
+pub struct PerfLog {
+
+}
